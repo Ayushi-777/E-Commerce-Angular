353b4b6954a40cba867593f72e41132340a588f6
Ayushi Gupta <ayushi.gupta2@incedonic.com>
2023-07-27 02:35:44 +0530

    Day2

--- E-Commerce-App/src/app/app.module.ts
+++ E-Commerce-App/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SellerHomeComponent } from './seller-home/seller-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    SellerAuthComponent
+    SellerAuthComponent,
+    SellerHomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- E-Commerce-App/src/app/seller-auth/seller-auth.component.ts
+++ E-Commerce-App/src/app/seller-auth/seller-auth.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SellerService } from '../services/seller.service';
+import { Router } from '@angular/router';
+import { SignUp } from '../data-type';
 
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
   styleUrls: ['./seller-auth.component.css']
 })
-export class SellerAuthComponent {
+export class SellerAuthComponent implements OnInit{
+constructor(private seller:SellerService,private router:Router){}
+ngOnInit(): void{}
+
+signUp(data:SignUp):void{
+  console.warn(data)
+  this.seller.userSignUp(data).subscribe((result)=>{
+ 
+    if(result){
+      this.router.navigate(['seller-home'])
+    }
+  });
+}
 
 }
